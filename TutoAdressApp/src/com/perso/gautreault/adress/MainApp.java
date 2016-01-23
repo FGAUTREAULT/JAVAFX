fa4e75a9edc7aaf69ac3c076446bf7969c1fbcff
FabienG <FabienG>
2016-01-23 18:26:41 +0100

    Style Css and Icons

--- TutoAdressApp/src/com/perso/gautreault/adress/MainApp.java
+++ TutoAdressApp/src/com/perso/gautreault/adress/MainApp.java
@@ -11,6 +11,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -54,6 +55,8 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+
+        this.primaryStage.getIcons().add(new Image("file:resources/images/Icon.png"));
 
         initRootLayout();
 
@@ -122,6 +125,8 @@ public class MainApp extends Application {
             dialogStage.initOwner(primaryStage);
             Scene scene = new Scene(page);
             dialogStage.setScene(scene);
+
+            dialogStage.getIcons().add(new Image("file:resources/images/Icon.png"));
 
             // Set the person into the controller.
             PersonEditDialogController controller = loader.getController();
